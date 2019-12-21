fe4c104264bf1f88c3e7cdc4f504e29f080d5122
wizzymore <bilucristian25@gmail.com>
2019-12-21 06:25:48 +0200

    Give module specific name

--- include.sh
+++ include.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-MOD_SKELETON_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
+MOD_INDIVIDUAL_XP_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
-source $MOD_SKELETON_ROOT"/conf/conf.sh.dist"
+source $MOD_INDIVIDUAL_XP_ROOT"/conf/conf.sh.dist"
 
-if [ -f $MOD_SKELETON_ROOT"/conf/conf.sh" ]; then
-    source $MOD_SKELETON_ROOT"/conf/conf.sh"
+if [ -f $MOD_INDIVIDUAL_XP_ROOT"/conf/conf.sh" ]; then
+    source $MOD_INDIVIDUAL_XP_ROOT"/conf/conf.sh"
 fi
